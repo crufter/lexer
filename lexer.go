23fe8c7add018ac7d37ceca2fa6c50d951ae0382
Dobronszki János <janosdobronszki@citromail.hu>
2012-09-07 07:34:43 +0200

    Gofmt.

--- lexer.go
+++ lexer.go
@@ -1,26 +1,26 @@
 package lexer
 
-import(
-	"regexp"
+import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
 type TokenExpr struct {
-	Regex	string
-	Typ		int
+	Regex string
+	Typ   int
 }
 
 type Token struct {
-	Text	string
-	Typ		int
-	Occ		int
-	Start	int
+	Text  string
+	Typ   int
+	Occ   int
+	Start int
 }
 
 type Tokens struct {
-	S 	[]Token
-	Pos	int
+	S   []Token
+	Pos int
 }
 
 func (t *Tokens) Has() bool {
@@ -53,7 +53,7 @@ func (t *Tokens) Until(i ...int) (Tokens, int) {
 	m := toMap(i...)
 	for i, v := range t.S[t.Pos+1:] {
 		if _, ok := m[v.Typ]; ok {
-			return Tokens{t.S[t.Pos+1:i-1],0}, i-1
+			return Tokens{t.S[t.Pos+1 : i-1], 0}, i - 1
 		}
 	}
 	return Tokens{}, -1
@@ -75,9 +75,9 @@ func LineAndPos(src string, pos int) (int, int) {
 // (This allows for example to do whitespace significant lexing.)
 func Lex(src string, tokens []TokenExpr) ([]Token, error) {
 	pos := 0
-    toks := []Token{}
+	toks := []Token{}
 	cache := []*regexp.Regexp{}
-	for ;pos < len(src); {
+	for pos < len(src) {
 		rem_src := src[pos:]
 		match := false
 		l := 0
@@ -94,16 +94,16 @@ func Lex(src string, tokens []TokenExpr) ([]Token, error) {
 			found := regex.FindIndex([]byte(rem_src))
 			if found != nil {
 				match = true
-				l = found[1]-found[0]
+				l = found[1] - found[0]
 				if tag > 0 {
 					tok := Token{rem_src[found[0]:found[1]], tag, 0, found[0]}
 					toks = append(toks, tok)
 				}
-				if tag < 0 {	// If tag is negative, then avoid repeating occurences and push only once.
-					if len(toks) == 0 || toks[len(toks)-1].Typ != tag {		// If the previous token is not the same type as the current, then push.
+				if tag < 0 { // If tag is negative, then avoid repeating occurences and push only once.
+					if len(toks) == 0 || toks[len(toks)-1].Typ != tag { // If the previous token is not the same type as the current, then push.
 						tok := Token{rem_src[found[0]:found[1]], tag, 1, found[0]}
 						toks = append(toks, tok)
-					} else {	// If its the same type, increase the occurence counter of the already pushed one.
+					} else { // If its the same type, increase the occurence counter of the already pushed one.
 						toks[len(toks)-1].Occ++
 					}
 				}
